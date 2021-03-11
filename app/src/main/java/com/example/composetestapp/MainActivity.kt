7a67a09c4e6f7f0750c25d3f7a17a867fec88f95
Alireza Rafeezadeh <alireza.mce@gmail.com>
2021-03-11 23:23:53 +0330

    Splash screen is done

--- app/src/main/java/com/example/composetestapp/MainActivity.kt
+++ app/src/main/java/com/example/composetestapp/MainActivity.kt
@@ -16,7 +16,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -155,18 +158,58 @@ fun BoxExample() {
             )
         }
 
-        Image(
-            painter = painterResource(id = R.drawable.splash_image),
-            contentDescription = null,
-            modifier = Modifier
-                .alpha(0.4f)
-                .height(370.dp)
-                .fillMaxWidth()
-                .clip(shape = RoundedCornerShape(4.dp))
-                .align(Alignment.BottomCenter),
-            contentScale = ContentScale.FillWidth
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
 
-        )
+
+
+            Image(
+                painter = painterResource(id = R.drawable.splash_image),
+                contentDescription = null,
+                modifier = Modifier
+                    .alpha(0.8f)
+                    .height(370.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp))
+                    .align(Alignment.Center),
+                contentScale = ContentScale.FillWidth
+
+            )
+            Box(
+//                painter = painterResource(id = R.drawable.splash_fade_image)
+//                , contentDescription = null,
+                modifier = Modifier
+//                    .background(shape = Rect)
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.TopCenter)
+
+            )
+
+            Box(
+                modifier = Modifier
+                    .height(140.dp)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.linearGradient(
+                            colors =
+//                            listOf(Color.Red, colorResource(id = R.color.teal_200)),
+                            listOf(Color.White, colorResource(id = R.color.blank)),
+                            start = Offset(0.0f, 0.0f),
+                            end = Offset(0.0f, 400.0f)
+                        )
+                    )
+//                    .drawBackground(
+//                        HorizontalGradient(
+//                            0.0f to Color.Red,
+//                            0.5f to Color.Green,
+//                            1.0f to Color.Blue,
+//                            startX = Px.Zero,
+//                            endX = 500.dp.toPx()
+//                        )
+//                    )
+            )
+
+        }
 
     }
 }
