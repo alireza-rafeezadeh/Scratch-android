1e1ed2628ff1920545c1a9b0eb383e169e0f2e69
Alireza Rafeezadeh <alireza.mce@gmail.com>
2021-03-11 22:52:58 +0330

    initial commit

---
+++ app/src/main/java/com/example/composetestapp/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.composetestapp
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composetestapp.ui.theme.ComposeTestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            ComposeTestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface {
+//                    Greeting("Android")
+//                    test()
+                    BoxExample()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var state = mutableStateOf("")
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hello $name!")
+//        Text(text = "wow" , style = TextStyle(fontSize = TextUnit.))
+        TextField(value = "sdsf", onValueChange = { /*TODO*/ })
+        OutlinedTextField(value = state.value, onValueChange = { state })
+//        OutlinedTextField(value = "sdsf", onValueChange = { /*TODO*/ })
+
+        Spacer(modifier = Modifier.height(100.dp))
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "press me!")
+        }
+
+//        Text(text = )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTestAppTheme {
+//        Greeting("Android")
+//        test()
+        BoxExample()
+    }
+}
+
+
+@Composable
+fun test() {
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Spacer(modifier = Modifier.fillMaxSize())
+
+        Image(
+            painter = painterResource(id = R.drawable.splash_image),
+            contentDescription = null,
+            modifier = Modifier
+                .height(270.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.FillWidth
+
+        )
+
+//        TextFieldDemo()
+
+
+    }
+
+}
+
+
+@Composable
+fun TextFieldDemo() {
+    Column(Modifier.padding(16.dp)) {
+        val textState = remember { mutableStateOf(TextFieldValue()) }
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            label = {
+                Text(text = "label")
+            }
+        )
+        Text("The textfield has this text: " + textState.value.text)
+    }
+}
+
+
+@Composable
+fun BoxExample() {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+//        Text("This text is drawn first", modifier = Modifier.align(Alignment.TopCenter))
+
+        Row(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(bottom = 160.dp)
+        ) {
+
+            Image(
+                painter = painterResource
+                    (id = R.drawable.ic_scratch_logo),
+                contentDescription = null,
+                Modifier
+                    .height(48.dp)
+                    .width(48.dp)
+
+            )
+
+            Text(
+                "Scratch", fontSize = 21.sp,
+                color = colorResource(id = R.color.text_color),
+                style = TextStyle(fontWeight = FontWeight.Bold),
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
+
+        Image(
+            painter = painterResource(id = R.drawable.splash_image),
+            contentDescription = null,
+            modifier = Modifier
+                .alpha(0.4f)
+                .height(370.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
+                .align(Alignment.BottomCenter),
+            contentScale = ContentScale.FillWidth
+
+        )
+
+    }
+}
