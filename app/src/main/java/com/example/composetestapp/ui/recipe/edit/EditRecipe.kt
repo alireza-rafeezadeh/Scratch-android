1bd716541cf3b20daf469d804c3b99cee3dc9243
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2021-03-15 16:55:53 +0330

    How to cook composabel added _SQ

--- app/src/main/java/com/example/composetestapp/ui/recipe/edit/EditRecipe.kt
+++ app/src/main/java/com/example/composetestapp/ui/recipe/edit/EditRecipe.kt
@@ -1,8 +1,7 @@
 package com.example.composetestapp.ui.recipe.edit
 
-import android.widget.Space
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,13 +13,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetestapp.ui.theme.ComposeTestAppTheme
+import com.example.composetestapp.ui.widgets.TextLead
 import com.example.composetestapp.R
 import com.example.composetestapp.ui.widgets.TextBody
 
@@ -57,8 +56,88 @@ fun EditRecipe() {
                     IngredientsItem()
                     Spacer(modifier = Modifier.padding(16.dp))
                 }
+
+                item {
+
+                    Card(
+                        elevation = 24.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 24.dp, end = 24.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, end = 16.dp)
+                        ) {
+
+                            Row {
+
+                                Box(
+                                    modifier = Modifier
+                                        .width(24.dp)
+                                        .height(24.dp)
+                                        .border(
+                                            width = 1.dp, color = colorResource(
+                                                id = R.color.jungle_green,
+                                            ), shape = RoundedCornerShape(50)
+                                        )
+                                ) {
+                                    Text(
+                                        fontSize = 11.sp,
+                                        modifier = Modifier.align(Alignment.Center),
+                                        text = "1", color = colorResource(
+                                            id = R.color.jungle_green
+                                        )
+                                    )
+                                }
+
+                                Spacer(Modifier.padding(16.dp))
+
+                                TextBody(
+                                    text = "Heat a Belgian waffle iron.",
+                                    modifier = Modifier.padding()
+                                )
+                            }
+
+
+                            Row {
+
+                                Box(
+                                    modifier = Modifier
+                                        .width(24.dp)
+                                        .height(24.dp)
+                                        .border(
+                                            width = 1.dp, color = colorResource(
+                                                id = R.color.jungle_green,
+                                            ), shape = RoundedCornerShape(50)
+                                        )
+                                ) {
+                                    Text(
+                                        fontSize = 11.sp,
+                                        modifier = Modifier.align(Alignment.Center),
+                                        text = "1", color = colorResource(
+                                            id = R.color.jungle_green
+                                        )
+                                    )
+                                }
+
+                                Spacer(Modifier.padding(16.dp))
+
+                                TextBody(
+                                    text = "Mix the flour, sugar, and baking powder together in a mixing bowl. Stir in 1 cup eggnog, butter, and the egg until well blended. Add more eggnog if needed to make a pourable batter.",
+                                    modifier = Modifier.padding()
+                                )
+                            }
+
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.padding(16.dp))
+                }
             })
-    } // end of column
+    }
 
 }
 
--- app/src/main/java/com/example/composetestapp/ui/recipe/edit/EditRecipeComponents.kt
+++ app/src/main/java/com/example/composetestapp/ui/recipe/edit/EditRecipeComponents.kt
@@ -1,22 +1,25 @@
 package com.example.composetestapp.ui.recipe.edit
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetestapp.R
 import com.example.composetestapp.ui.widgets.H5Text
+import com.example.composetestapp.ui.widgets.TextBody
 
 
 @Composable
-fun EditRecipeCardHeader(title : String) {
+fun EditRecipeCardHeader(title: String) {
 
 
     Row(
@@ -35,6 +38,40 @@ fun EditRecipeCardHeader(title : String) {
             painter = painterResource(id = R.drawable.ic_edit),
             contentDescription = null,
             contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+
+@Composable
+fun HowToCookItem(description: String, itemNumber: Int) {
+    Row {
+
+        Box(
+            modifier = Modifier
+                .width(24.dp)
+                .height(24.dp)
+                .border(
+                    width = 1.dp, color = colorResource(
+                        id = R.color.jungle_green,
+                    ), shape = RoundedCornerShape(50)
+                )
+        ) {
+            Text(
+                fontSize = 11.sp,
+                modifier = Modifier.align(Alignment.Center),
+                text = itemNumber.toString(),
+                color = colorResource(
+                    id = R.color.jungle_green
+                )
+            )
+        }
+
+        Spacer(Modifier.padding(16.dp))
+
+        TextBody(
+            text = description,
+            modifier = Modifier.padding()
         )
     }
 }
